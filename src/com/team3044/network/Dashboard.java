f855c78079a4fcbbe750184d767dc33b958f0171
Joey <Joey@Joeys-Laptop>
2014-02-05 13:15:19 -0500

    modified dashboard, and added class for managing camera

--- src/com/team3044/network/Dashboard.java
+++ src/com/team3044/network/Dashboard.java
@@ -6,10 +6,57 @@
 
 package com.team3044.network;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 /**
  *
  * @author Joey
  */
 public class Dashboard {
+    private static Dashboard dashboard;
+    
+    public Dashboard(){
+        
+    }
+    
+    public void putDouble(String tag, double value){
+        tag = tag.toUpperCase();
+        SmartDashboard.putNumber(tag, value);
+    }
+    
+    public void putString(String tag, String value){
+        tag = tag.toUpperCase();
+        SmartDashboard.putString(tag, value);
+    }
+    
+    public void putBoolean(String tag, boolean bool){
+        tag = tag.toUpperCase();
+        SmartDashboard.putBoolean(tag, bool);
+    }
+    
+    public double getDouble(String tag, double defaultValue){
+        tag = tag.toUpperCase();
+        double val = SmartDashboard.getNumber(tag,defaultValue);
+        return val;
+    }
+    
+    public String getString(String tag){
+        tag = tag.toUpperCase();
+        return SmartDashboard.getString(tag, "");
+    }
+    
+    public boolean getBoolean(String tag, boolean defaultValue){
+        tag = tag.toUpperCase();
+        return SmartDashboard.getBoolean(tag,defaultValue);
+    }
+    
+    
+    
+    public static Dashboard getInstance(){
+        if(dashboard == null){
+            dashboard = new Dashboard();
+        }
+        return dashboard;
+    }
     
 }
